4eb6612bc908d15f2c02121549dcc0c09bb438d9
NyanGuy <vasyokbelyivnew@gmail.com>
2018-10-04 19:38:11 +0500

    Fixing particles.js on github pages

--- media/js/app.js
+++ media/js/app.js
@@ -1,7 +1,7 @@
 jQuery(document).ready( function() {
     
     /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-    particlesJS.load('particles-js', '/media/assets/particles/particlesjs-config.json', function() {
+    particlesJS.load('particles-js', 'https://rawgit.com/NyanSandbox/what-is-your-name/master/media/assets/particles/particlesjs-config.json', function() {
         console.log('callback - particles.js config loaded');
     });
 
