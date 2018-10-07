aeb0a7a13e8fb27f6148e5a792f3ea3a7c8d890e
NyanGuy <vasyokbelyivnew@gmail.com>
2018-10-07 21:34:16 +0500

    Add little feature >:з

--- media/js/app.js
+++ media/js/app.js
@@ -3,12 +3,12 @@ jQuery(document).ready( function() {
     /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
     particlesJS.load('particles-js', 'https://rawgit.com/NyanSandbox/what-is-your-name/master/media/assets/particles/particlesjs-config.json', function() {
         console.log(`
-        / Sex, drugs & rock'n'roll? — Bugs, hex & \
-        \ source control                          /
+        / Sex, drugs & rock'n'roll? — Bugs, hex & \\
+        \\ source control                          /
          -----------------------------------------
-         \
-          \
-           \ >()_
+         \\
+          \\
+           \\ >()_
               (__)__ _
 `);
     });
