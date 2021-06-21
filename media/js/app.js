62f5279eeb1e788162b83eae026afd49db675e6a
Vasiliy <vasyokbelyivnew@gmail.com>
2021-06-21 23:32:29 +0500

    Added easter egg

--- media/js/app.js
+++ media/js/app.js
@@ -1,9 +1,9 @@
 $(document).ready( function() {
-    
+
     /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
     // Load Particles.JS with custom options
     particlesJS.load('particles-js', 'https://rawgit.com/NyanSandbox/what-is-your-name/master/media/assets/particles/particlesjs-config.json', function() {
-        console.log(` 
+        console.log(`
         / Sex, drugs & rock'n'roll? — Bugs, hex & \\
         \\ source control                          /
          -----------------------------------------
@@ -14,18 +14,32 @@ $(document).ready( function() {
         `);
     });
 
+    function createMessage(name) {
+      switch (name.toLowerCase()) {
+        case 'вишенка':
+        case 'дина':
+          return ':3 Ты мне нравишься, ';
+          break;
+
+        default:
+          return 'Ты никому не нравишься, ';
+          break;
+      }
+    }
+
     // When click on success button
     $( '#success' ).click( function(){
         // def name of user
         const name = $('#nameInput').val();
-        
+
         // Open modal with troll >:D
         $('#bu-ga-ga').modal({keyboard : false})
         // Remove text in modal, if exist
         $('#NoOneLikesYou').text('');
         // Every .5 sec add message to modal
+        let message = createMessage(name);
         let interval = setInterval(function(){
-          $('#NoOneLikesYou').append('Ты никому не нравишься, ' + name + '!<br>');
+          $('#NoOneLikesYou').append(message + name + '!<br>');
         }, 500);
         // when modal hidden
         $('#bu-ga-ga').on('hidden.bs.modal', function (e) {
